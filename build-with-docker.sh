5d2cffe9953eafae3e3f00d62ccf09c5e8533353
Rémi AUGUSTE <remi.auguste@gmail.com>
2022-12-20 19:45:29 +0100

    cuda build with cudnn

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -3,7 +3,7 @@
 # This is usefull if you want to use OpenCV in another docker container with CUDA
 
 docker rm build-opencv
-docker run -it --name build-opencv -v $PWD/opencv:/var/build/opencv -v $PWD/opencv_contrib:/var/build/opencv_contrib -v $PWD/makefile:/var/build/makefile nvidia/cuda:11.8.0-devel-ubuntu18.04 /bin/bash -c "cd /var/build; \
+docker run -it --name build-opencv -v $PWD/opencv:/var/build/opencv -v $PWD/opencv_contrib:/var/build/opencv_contrib -v $PWD/makefile:/var/build/makefile nvidia/cuda:11.8.0-cudnn8-devel-ubuntu18.04 /bin/bash -c "cd /var/build; \
 	apt-get update; \
 	DEBIAN_FRONTEND=noninteractive make dep; \
 	make clean; \
